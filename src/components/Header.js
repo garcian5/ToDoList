357fee5b325ffee688fa9c2c837c987020e76318
Nheljee Rose Garcia <garcian5@mymacewan.ca>
2020-09-27 19:21:57 -0600

    Added a font and adjusted some styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,28 +7,24 @@ function Header() {
 	let hod = hrOfDay();
 
 	let cLstStyles = {
-		backgroundColor: "rgba(186, 152, 3, 0.32)",		
-		marginRight: 250,
-		marginLeft: 250,
-		marginTop: 50,
-		marginBottom: 50,
-		padding: 20,
-		fontFamily: "Roboto, sans-serif",
-		fontWeight: 100,
-		fontSize: 15,
-		color: "#666666",
+		backgroundColor: "rgba(186, 152, 3, 0.32)",	
+		margin: "auto",
+		height: 250,
+		width: 350,		
 		borderRadius: "50%"
 	}
 
 	// change background color depending on the times of the day
 	if (tod === "afternoon") cLstStyles.backgroundColor = "rgba(208, 6, 6, 0.32)";
-	else if (tod === "night") cLstStyles.backgroundColor = "rgba(2, 1, 60, 0.32)";
+	else if (tod === "night") cLstStyles.backgroundColor = "rgba(45, 4, 195, 0.32)";
 
 	return (
 		<header style={cLstStyles}>
-			<h1>Good {tod}!</h1>
-			<h4>It is currently {hod} where you are.</h4>
-			<h4>Here is your To Do List.</h4>
+			<div className="greeting-header">
+				<h1>Good {tod}!</h1>
+				<h4>It is currently {hod} where you are.</h4>
+				<h4>Here is your To Do List.</h4>
+			</div>
 		</header>
 		)
 }
