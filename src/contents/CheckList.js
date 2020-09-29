3c7c945ed34a0b7228470151f0eadec576d9962b
Nheljee Rose Garcia <garcian5@mymacewan.ca>
2020-09-28 21:02:40 -0600

    Added an event handler for checkbox

--- src/contents/CheckList.js
+++ src/contents/CheckList.js
@@ -1,13 +1,32 @@
 import React from "react"
 
 class CheckList extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            complete: false
+        }
+        // bind the method made to our own class to avoid a typeerror
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    /**
+     * A method that handles the if the checkbox is clicked or not.
+     * Changes the state of the box to clicked or not clicked.
+     * */
+    handleChange() {
+        this.setState({ complete: !this.state.complete })
+    }
+
     render() {
         return (
             <div>
 
                 <div className="todo-item">
                     <label id="label1">
-                        <input className="chk-box" type="checkbox" checked={this.props.data.completed} />
+                        <input className="chk-box" type="checkbox"
+                            onChange={this.handleChange}
+                            defaultChecked={this.props.data.completed} />
                         {this.props.data.text}
                     </label><br />
                 </div>
