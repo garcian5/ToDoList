d4bfdacbebe1d5f61b1c4424e0116b7cadc994db
Nheljee Rose Garcia <garcian5@mymacewan.ca>
2020-10-15 18:54:38 -0600

    got rid of border

--- src/contents/ProgressReport.js
+++ src/contents/ProgressReport.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Button from 'react-bootstrap/Button';
+//import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import ProgressSummary from "./ProgressSummary";
 
