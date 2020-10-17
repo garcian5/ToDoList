218cf7442ca1cc71a38ac07588f90a816b50952a
Nheljee Rose Garcia <garcian5@mymacewan.ca>
2020-10-16 21:12:09 -0600

    added a rel to solve security risk warning

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,9 +8,9 @@ class Footer extends React.Component {
                     <div className="col">
                         <ul className="social-links">
                             {/*target = "_blank" will open link to new tab*/}
-                            <li><a href="https://github.com/garcian5/ToDoList" target="_blank" title="Nheljee's Github"><ion-icon name="logo-github"></ion-icon></a></li>
-                            <li><a href="https://www.linkedin.com/in/nheljee-rose-garcia/" target="_blank" title="Nheljee's LinkedIn"><ion-icon name="logo-linkedin"></ion-icon></a></li>
-                            <li><a href="https://nheljee-dice-game.netlify.app/" target="_blank" title="Other Websites"><ion-icon name="apps-outline"></ion-icon></a></li>
+                            <li><a href="https://github.com/garcian5/ToDoList" rel="noopener noreferrer" target="_blank" title="Nheljee's Github"><ion-icon name="logo-github"></ion-icon></a></li>
+                            <li><a href="https://www.linkedin.com/in/nheljee-rose-garcia/" rel="noopener noreferrer" target="_blank" title="Nheljee's LinkedIn"><ion-icon name="logo-linkedin"></ion-icon></a></li>
+                            <li><a href="https://nheljee-dice-game.netlify.app/" rel="noopener noreferrer" target="_blank" title="Other Websites"><ion-icon name="apps-outline"></ion-icon></a></li>
                         </ul>
                     </div>
                 </div>
