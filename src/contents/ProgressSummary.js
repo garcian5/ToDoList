49c43691929e9fd962738ee09ffe243dce9019e4
Nheljee Rose Garcia <garcian5@mymacewan.ca>
2020-10-18 15:11:29 -0600

    converted Progress Summary & Report to functional component

--- src/contents/ProgressSummary.js
+++ src/contents/ProgressSummary.js
@@ -1,15 +1,13 @@
 import React from "react";
 
-class ProgressSummary extends React.Component {
-    render() {
-        return (
-            <section>
-                <ul>
-                    <li>{this.props.data.text}</li>
-                </ul>                
-            </section>
-        )
-    }
+function ProgressSummary (props) {
+    return (
+        <section>
+            <ul>
+                <li>{props.data.text}</li>
+            </ul>                
+        </section>
+    )
 }
 
 export default ProgressSummary;
