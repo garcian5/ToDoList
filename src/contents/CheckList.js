30c804070dc7a9e47f6ac775b610d983a5599d30
Nheljee Rose Garcia <garcian5@mymacewan.ca>
2020-10-18 15:03:11 -0600

    converted CheckList.js to functional component

--- src/contents/CheckList.js
+++ src/contents/CheckList.js
@@ -1,30 +1,28 @@
 import React from "react";
 
-class CheckList extends React.Component {
-    render() {
-        // styles that will be invoked if the task is completed
-        const completedStyle = {
-            textDecoration: "line-through",
-            fontStyle: "italic",
-            fontWeight: 300,
-            color: "#C0C0C0"
-        };
+function CheckList(props) {    
+    // styles that will be invoked if the task is completed
+    const completedStyle = {
+        textDecoration: "line-through",
+        fontStyle: "italic",
+        fontWeight: 300,
+        color: "#C0C0C0"
+    };
 
-        return (            
-            <div>
-                <div className="todo-item">                    
-                    <label style={this.props.data.completed ? completedStyle : null}>
-                        <input type="checkbox"
-                            name={this.props.data.id}
-                            checked={this.props.data.completed}
-                            onChange={this.props.handleChange}
-                        />
-                        {this.props.data.text}
-                    </label>
-                </div>                
-            </div>
-        )
-    }
+    return (            
+        <div>
+            <div className="todo-item">                    
+                <label style={props.data.completed ? completedStyle : null}>
+                    <input type="checkbox"
+                        name={props.data.id}
+                        checked={props.data.completed}
+                        onChange={props.handleChange}
+                    />
+                    {props.data.text}
+                </label>
+            </div>                
+        </div>
+    )
 }
 
 export default CheckList;
